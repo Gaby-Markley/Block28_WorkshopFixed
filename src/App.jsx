4d8579eeda8c128ff7c210bf311b0508f1c9828a
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-08-15 22:06:05 -0700

    got up to tab 3 #10

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Blue from "./components/blue.jsx";
 import Red from "./components/red.jsx";
+import Home from "./components/home.jsx";
 
 import "./App.css";
 
@@ -13,12 +14,14 @@ function App() {
         <div id="navbar">
           <Link to="/blue">Blue </Link>
           <Link to="/red"> Red </Link>
+          <Link to="/home">Home </Link>
         </div>
         <h1> Colors </h1>
         <div id="main-section">
           <Routes>
             <Route path="/blue" element={<Blue />} />
             <Route path="/red" element={<Red />} />
+            <Route path="/home" element={<Home />} />
           </Routes>
         </div>
       </div>
